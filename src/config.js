e285f2b88f0fc02891a300733061883b22f996d6
Erick Bernardo <erickvictor@gmail.com>
2018-07-01 22:08:36 -0400

    Add webpack to compile to umd

--- src/config.js
+++ src/config.js
@@ -1,1 +1,9 @@
+const TOKEN_API = 'BQBiqxkk0oHzkq2Do4odNrSX6z-_jMoMcovUKzoWRyhs6y7Uq4mMI3pkGO6lngtwAm8Xt3W8kCmeY3E2XBWUAkGeIjnqZmXtHJcbLtW6dhHlPhOXmrSbxjXHh5tWL-cK0pZ7wNKW63kaGp4';
+
 export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `'Bearer ${TOKEN_API}'`,
+  },
+};
