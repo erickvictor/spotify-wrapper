aa88297e07d96375474a9774198e9c64e0c8c9f5
Erick Bernardo <erickvictor@gmail.com>
2018-07-26 21:44:45 -0400

    Refactor search method

--- examples/albums.js
+++ examples/albums.js
@@ -1,7 +1,11 @@
 global.fetch = require('node-fetch');
 
-import { searchAlbums } from '../src/main';
+import SpotifyWrapper from '../src/index';
 
-const albums = searchAlbums('Incubus');
+const spotify = new SpotifyWrapper({
+  token: 'BQAfhdDi5WBSWCRXt92mSk7D0UOC63LLm0J8yJKVN197V32xMO64rjpejTw0U4l54NOd5kGtxPOYpz4iVJ8iTJyPZjnr-1-zZa-FFnu0Lo4ej9V28XDbABBUhAZTNLR8vq6X3ArTUD5HEzU'
+})
+
+const albums = spotify.search.albums('Incubus');
 
 albums.then(data => data.albums.items.map(item => console.log(item.name)));
