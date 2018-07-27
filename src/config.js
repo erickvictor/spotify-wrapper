1303174dc048d739f605be9608a3100ebb411f24
Erick Bernardo <erickvictor@gmail.com>
2018-07-26 21:09:44 -0400

    Refactor album methods

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const TOKEN_API = 'BQAATcewWTreiJuALH_H1XlY5r7tACNeTg6FDSYbNeJgJpAQ6EEam3P-uDi6b9UhNMe1UqZLYlOZEQHxnq1swCTS_isBpghwvweOetJrmQ4KpPaxxeneLnriaKhLjmObJ0kXsWFHum_ZS6E';
+const TOKEN_API = 'BQAfhdDi5WBSWCRXt92mSk7D0UOC63LLm0J8yJKVN197V32xMO64rjpejTw0U4l54NOd5kGtxPOYpz4iVJ8iTJyPZjnr-1-zZa-FFnu0Lo4ej9V28XDbABBUhAZTNLR8vq6X3ArTUD5HEzU';
 
 export const API_URL = 'https://api.spotify.com/v1';
 
